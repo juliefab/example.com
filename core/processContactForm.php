b36ab3137ab31ac936a5b6191ed3dcf1a180ff49
juliefab <jfinch1@artic.edu>
2017-12-15 16:23:57 -0600

    wired up mailgun

--- core/processContactForm.php
+++ core/processContactForm.php
@@ -2,10 +2,11 @@
 
 //Include non-vendor files
 require '../core/About/src/Validation/Validate.php';
-
+require '../vendor/autoload.php';
+require '../../keys.php';
 //Declare Namespaces
 use About\Validation;
-
+use Mailgun\Mailgun;
 //Validate Declarations
 $valid = new About\Validation\Validate();
 $input = filter_input_array(INPUT_POST);
@@ -43,6 +44,20 @@ if(!empty($input)){
 
     if(empty($valid->errors) && !empty($input)){
         $message = "<div style=\"color:#00ff00;\">Success!Your form has been submitted.</div>";
+
+        # Instantiate the client.
+$mgClient = new Mailgun($mailgunkey);
+$domain = "sandbox6c420695c3e04d45a74bfa75cb3be647.mailgun.org";
+
+# Make the call to the client.
+$result = $mgClient->sendMessage("$domain",
+          array('from'    => "{$input['first_name']} {$input['last_name']} <{$input["email"]}>",
+                'to'      => 'Julie <juliefab2009@gmail.com>',
+                'subject' =>  $input ['subject'],
+                'text'    =>  $input ['message']
+            ));
+
+var_dump($result);
     }else{
         $message = "<div style=\"color:#ff0000;\">Error!Please correct the errors below.</div>";
 }
