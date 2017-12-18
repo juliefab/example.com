d04b20beaff4aab27a8ce8976c49afcf6e3333fb
juliefab <jfinch1@artic.edu>
2017-12-18 16:56:59 -0600

    added layout.template changed html to php

--- public/contact.php
+++ public/contact.php
@@ -1,61 +1,43 @@
-<?php require '../core/processContactForm.php' ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-    <head>
-      <meta charset="UTF-8">
-       <title>Contact</title>
-       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-       <style>
-
-       </style>
-    </head>
-    <body>
-        <nav>
-      <a href="index.html">Home</a> | <a href="contact.php">Contact</a> | <a href="resume.html">Resume</a>
-    </nav>
-
-          <h1>Contact Julie Finch</h1>
-            <?php echo $message ?>
+<?php
+require '../core/processContactForm.php';
 
 
+    $content = <<<EOT
+        <h1 id = "header" class="header">Contact Julie </h1>
+        {$message}
           <form method="post">
             <div>
               <label for="firstName">First Name</label><br>
               <input type="text" name="first_name" id="firstName">
-              <div style="color: #ff0000;"><?php echo $valid->error('first_name'); ?></div>
+              <div style="color: #ff0000;">{$valid->error('first_name')}</div>
             </div>
 
             <div>
               <label for="lastName" id="lastName">Last Name</label><br>
               <input type="text" name="last_name">
-               <div style="color: #ff0000;"><?php echo $valid->error('last_name'); ?></div>
+               <div style="color: #ff0000;">{$valid->error('last_name')}</div>
             </div>
 
             <div>
               <label for="email" id="email">Email</label><br>
               <input type="text" name="email">
-               <div style="color: #ff0000;"><?php echo $valid->error('email'); ?></div>
+               <div style="color: #ff0000;">{$valid->error('email')}</div>
             </div>
 
             <div>
               <label for="subject" id="subject">Subject</label><br>
               <input type="text" name="subject">
-               <div style="color: #ff0000;"><?php echo $valid->error('subject'); ?></div>
+               <div style="color: #ff0000;">{$valid->error('subject')}</div>
             </div>
 
             <div>
               <label for="message" id="message">Message</label><br>
               <textarea name="message"></textarea>
-               <div style="color: #ff0000;"><?php echo $valid->error('message'); ?></div>
+               <div style="color: #ff0000;">{$valid->error('message')}</div>
             </div>
 
             <input type="submit">
+      </form>
+EOT;
 
-          </form>
-        </body>
-      </html>
-
-    </body>
-</html>
+require '../core/layout.php';
